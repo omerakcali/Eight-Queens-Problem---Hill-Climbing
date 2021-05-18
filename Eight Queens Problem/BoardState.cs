a9fd679d396875f20f5db574d5eb9855e04e5e49
Ömer Akçalı <omerakcali99@gmail.com>
2021-05-18 14:49:53 +0300

    Full Upload

---
+++ Eight Queens Problem/BoardState.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eight_Queens_Problem
+{
+    class BoardState
+    {
+        public int[] Board;
+        
+        public int fitness;
+        public BoardState()
+        {
+            Board = new int[8];
+            fitness = int.MaxValue;
+        }
+        public void Fill(int[] indexes)
+        {
+            Board = indexes;
+            CalculateFitness();
+        }
+        public void RandomFill()
+        {
+            Random rand = new Random();
+            int[] indexes = new int[8];
+            for(int i =0; i < 8; i++)
+            {
+                indexes[i] = rand.Next(0, 8);
+            }
+            Fill(indexes);
+        }
+        void CalculateFitness()
+        {
+            int fitness = 0;
+            for(int i =0; i < 8; i++)
+            {
+                for(int j = i+1; j < 8; j++)
+                {
+                    if (Board[i] == Board[j]) fitness++;
+                    if (Math.Abs(Board[i] - Board[j]) == Math.Abs(i - j)) fitness++;
+                }
+            }
+            this.fitness = fitness;
+
+        }
+
+        public void PrintState()
+        {
+            String boardString = "";
+                for(int i =0; i < 8; i++)
+            {
+                for(int j =0; j < 8; j++)
+                {
+                    if (Board[j] == i) boardString += 'X';
+                    else boardString += "O";
+                    boardString += " ";
+                }
+                boardString += "\n";
+            }
+            Console.WriteLine(boardString);
+            Console.WriteLine("Fitness= " + fitness);
+            
+        }
+
+        public BoardState[] GetNeighborStates()
+        {
+            BoardState[] nei = new BoardState[56];
+            int addindex = 0;
+            for(int i =0; i < 8; i++)
+            {
+                for(int j =0; j < 8; j++)
+                {
+                    if (j == Board[i]) continue;
+                    BoardState tempNeighbor = new BoardState();
+                    int[] indexes = new int[8];
+                    Array.Copy(Board, indexes, 8);
+                    indexes[i] = j;
+                    tempNeighbor.Fill(indexes);
+                    nei[addindex] = tempNeighbor;
+                    addindex++;
+
+                }
+            }
+            
+            return nei.OrderBy(x => x.fitness).ToArray();
+        }
+
+    }
+}
---
+++ Eight Queens Problem/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Eight_Queens_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //GetNeighborStates fonksiyonunun ilk çalıştırıldığında fazladan zaman harcadığını tespit ettik
+            //Bunun sistemin hafıza allocate etmesiyle ilgili olduğunu tahmin ediyoruz.
+            //Bu yüzden doğru zaman ölçümü yapabilmek için en başta bir kere çağırıyoruz
+            // Bunu yapmazsak 25 döngünün ilk döngüsündeki HillClimb işlemi olması gerekenden çok daha uzun sürüyor
+            // Ve ortalama istatistiklerini bozuyor.
+            BoardState state = new BoardState();
+            state.RandomFill();
+            state.GetNeighborStates ();
+
+            int ClimbCount = 25;
+            double[][] infos = new double[ClimbCount+1][];
+            double moveSum = 0;
+            double resSum = 0;
+            double timeSum = 0;
+            for (int i =0; i < ClimbCount; i++)
+            {
+                BoardState startState = new BoardState();
+                infos[i] = new double[3];
+                Console.WriteLine("STARTING STATE\n----------------");
+                startState.RandomFill();
+                startState.PrintState();
+                startState = HillClimb(startState, out infos[i][0], out infos[i][1], out infos[i][2]);
+                Console.WriteLine("\nPROBLEM SOLVED\n----------------");
+                startState.PrintState();
+                moveSum += infos[i][0];
+                resSum += infos[i][1];
+                timeSum += infos[i][2];
+                Console.WriteLine("\nRUN " + (i + 1) + " MoveCounts: " + infos[i][0] + " ResCounts: " + infos[i][1] + " RunTime: " + infos[i][2] + "µs");
+                Console.WriteLine("###########################################################\n");
+            }
+            infos[ClimbCount] = new double[3];
+            infos[ClimbCount][0]=moveSum/ClimbCount;
+            infos[ClimbCount][1]=resSum/ClimbCount;
+            infos[ClimbCount][2]=timeSum/ClimbCount;
+            Console.WriteLine();
+            Console.WriteLine("AVERAGES: Moves: " + infos[ClimbCount][0] + " Random Restarts: " + infos[ClimbCount][1] + " Run Times: " + infos[ClimbCount][2]);
+            
+        }
+        static BoardState HillClimb(BoardState state, out double moves, out double randomRestarts, out double time)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Reset();
+            watch.Start();
+            BoardState[] successors = new BoardState[56];
+            
+            int moveCount = 0;
+            int randomRestartCount = 0;
+
+            int it = 0;
+            while(state.fitness != 0)
+            {
+                
+                successors = state.GetNeighborStates();
+                BoardState bestSuccessor = selectBestSuccessor(successors);
+                moveCount++;
+                if (bestSuccessor.fitness < state.fitness)
+                {
+                    state = bestSuccessor;
+                }
+                else {
+                    state.RandomFill();
+                    randomRestartCount++;
+                }
+
+            }
+            moves = moveCount;
+            randomRestarts = randomRestartCount;
+            watch.Stop();
+            time = Convert.ToDouble(  watch.ElapsedTicks / (System.Diagnostics.Stopwatch.Frequency/(1000L*1000L)));
+            return state;
+        }
+
+        static BoardState selectBestSuccessor(BoardState[] successors)
+        {
+            List<BoardState> bests = new List<BoardState>();
+            int minval = successors[0].fitness;
+            foreach(BoardState i in successors)
+            {
+                if (i.fitness > minval) break;
+                bests.Add(i);
+            }
+            Random rand = new Random();
+            return bests[rand.Next(0, bests.Count)];
+        }
+    }
+
+    
+}
